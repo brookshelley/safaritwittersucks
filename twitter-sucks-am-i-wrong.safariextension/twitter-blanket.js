04b056401b7d879a907c8a1bbc5ba9139cd0b928
brookshelley <brookshelley@gmail.com>
2016-11-13 14:45:18 -0800

    twitterblock

---
+++ twitter-sucks-am-i-wrong.safariextension/twitter-blanket.js
@@ -0,0 +1,8 @@
+$(function() {
+
+  coverMeLikeABlanket = function() {
+    $('head').append('<style id="everyoneChill">.dashboard-left,.dashboard-right,.moments,.SidebarCommonModules { display: none };</style>');
+  };
+
+  coverMeLikeABlanket(".dashboard-left", '');
+});
